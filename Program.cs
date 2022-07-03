b0a34452ca1e14eecf468fd5fef17442978c5249
Maruthi Akula <maruthiraoakula22@gmail.com>
2022-07-03 22:35:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Static_And_Instance
+{
+    internal class Program
+    {
+        public int add(int a, int b)
+        {
+            return a + b;
+        }
+        public static void method(int target)
+        {
+            int start = 0;
+            while (start <= target)
+            {
+                Console.WriteLine("start={0}",start);
+                start = start + 2;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+           int sum= obj.add(1, 2);
+            Console.WriteLine("sum ={0}",sum);
+            Program.method(20);
+        }
+    }
+}
